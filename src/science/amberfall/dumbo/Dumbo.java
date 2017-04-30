ce45c9734df23b496418f338536b8d8a0b2dce88
Sweepyoface <github@sweepy.pw>
2017-04-29 22:33:08 -0400

    Formatting

--- src/science/amberfall/dumbo/Dumbo.java
+++ src/science/amberfall/dumbo/Dumbo.java
@@ -20,99 +20,93 @@ import org.bukkit.event.Listener;
 
 public class Dumbo extends JavaPlugin implements Listener {
 
-	@Override
-	public void onEnable() {
-		createConfig();
-		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    @Override
+    public void onEnable() {
+        createConfig();
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
 
-		if (Bukkit.getServer().getVersion().contains("git-Spigot")) {
-			getLogger().info(ChatColor.RED + "We highly recommend that you use Paper over Spigot, as they have been hostile to me personally and the development of this plugin. Paper offers many performance improvements and optimizations over Spigot. Please download and install Paper from https://paperci.emc.gs to use this plugin.");
-			Bukkit.getPluginManager().disablePlugin(this);
-		}
+        if (Bukkit.getServer().getVersion().contains("git-Spigot")) {
+            getLogger().info(ChatColor.RED + "We highly recommend that you use Paper over Spigot, as they have been hostile to me personally and the development of this plugin. Paper offers many performance improvements and optimizations over Spigot. Please download and install Paper from https://paperci.emc.gs to use this plugin.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
 
-	}
+    }
 
-	@Override
-	public void onDisable() {
-	}
+    @Override
+    public void onDisable() {}
 
-	private void createConfig() {
-		try {
-			if (!getDataFolder().exists()) {
-				getDataFolder().mkdirs();
-			}
-			File file = new File(getDataFolder(), "config.yml");
-			if (!file.exists()) {
+    private void createConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
 
-				getLogger().info("Fetching quotes from GitHub..");
+                getLogger().info("Fetching quotes from GitHub..");
 
-				InputStream in = new URL("https://raw.githubusercontent.com/sweepyoface/dumbo/master/quotes.yml")
-						.openStream();
-				YamlConfiguration yaml = YamlConfiguration.loadConfiguration(new InputStreamReader(in));
+                InputStream in = new URL("https://raw.githubusercontent.com/sweepyoface/dumbo/master/quotes.yml").openStream();
+                YamlConfiguration yaml = YamlConfiguration.loadConfiguration(new InputStreamReader( in ));
 
-				String color = "&6";
+                String color = "&6";
 
-				this.getConfig().addDefault("color", color);
-				this.getConfig().addDefault("quotes", yaml.getStringList("quotes"));
+                this.getConfig().addDefault("color", color);
+                this.getConfig().addDefault("quotes", yaml.getStringList("quotes"));
 
-				this.getConfig().options().copyDefaults(true);
-				saveConfig();
-				reloadConfig();
+                this.getConfig().options().copyDefaults(true);
+                saveConfig();
+                reloadConfig();
 
-				getLogger().info("Done!");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                getLogger().info("Done!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String randomQuote() {
+    public String randomQuote() {
 
-		Random random = new Random();
-		List<String> quotes = this.getConfig().getStringList("quotes");
-		Integer quote = random.nextInt(quotes.size());
+        Random random = new Random();
+        List < String > quotes = this.getConfig().getStringList("quotes");
+        Integer quote = random.nextInt(quotes.size());
 
-		return (String) quotes.get(quote);
-	}
+        return (String) quotes.get(quote);
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-		Player player = (Player) sender;
+        Player player = (Player) sender;
 
-		if (label.equalsIgnoreCase(("dumbo")) && sender instanceof Player) {
-			if (args.length == 0) {
-				if (player.hasPermission("dumbo.quote") || player.isOp()) {
-					Bukkit.getServer().broadcastMessage(
-							ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("color"))
-									+ randomQuote());
-				}
-			} else if (args[0].equalsIgnoreCase("reload")) {
-				if (player.hasPermission("dumbo.reload") || player.isOp()) {
-					this.reloadConfig();
-					player.sendMessage(ChatColor.GREEN + "Dumbo config reloaded!");
-				}
-			} else {
-				player.sendMessage(ChatColor.RED + "Unknown argument. See /help dumbo");
-			}
-		}
-		return true;
-	}
+        if (label.equalsIgnoreCase(("dumbo")) && sender instanceof Player) {
+            if (args.length == 0) {
+                if (player.hasPermission("dumbo.quote") || player.isOp()) {
+                    Bukkit.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("color")) + randomQuote());
+                }
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                if (player.hasPermission("dumbo.reload") || player.isOp()) {
+                    this.reloadConfig();
+                    player.sendMessage(ChatColor.GREEN + "Dumbo config reloaded!");
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "Unknown argument. See /help dumbo");
+            }
+        }
+        return true;
+    }
 
-	@EventHandler
-	public void onChat(AsyncPlayerChatEvent ev) {
-		if (!ev.isCancelled()) {
-			String[] msgArray = ev.getMessage().trim().split("\\s+");
-			if (msgArray[0].equalsIgnoreCase(".dumbo")) {
-				Player player = (Player) ev.getPlayer();
-				if (player.hasPermission("dumbo.quote") || player.isOp()) {
-					Bukkit.getServer().broadcastMessage(
-							ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("color"))
-									+ randomQuote());
-				} else {
-					player.sendMessage(ChatColor.DARK_RED + "You do not have access to that command.");
-				}
-			}
-		}
-	}
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent ev) {
+        if (!ev.isCancelled()) {
+            String[] msgArray = ev.getMessage().trim().split("\\s+");
+            if (msgArray[0].equalsIgnoreCase(".dumbo")) {
+                Player player = (Player) ev.getPlayer();
+                if (player.hasPermission("dumbo.quote") || player.isOp()) {
+                    Bukkit.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("color")) + randomQuote());
+                } else {
+                    player.sendMessage(ChatColor.DARK_RED + "You do not have access to that command.");
+                }
+            }
+        }
+    }
 }
